0e1421541628f21b478fda69a1d1a0416b912f4d
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2019-05-01 00:24:03 -0500

    Entradas del Usuario

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -1,7 +1,7 @@
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-//Clase para las entradas del usuario al usar las teclas
+//Clase para las entradas del teclado
 public class KeyInput extends KeyAdapter{
 
 	private Handler handler;
@@ -13,10 +13,37 @@ public class KeyInput extends KeyAdapter{
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		
-		System.out.println(key);
+		for(int i =0; i<handler.objeto.size();i++) { //tempObject es cualquier objecto en nuestro juego
+			GameObject tempObject = handler.objeto.get(i);
+			
+			if(tempObject.getId() == ID.Jugador) {
+				//Eventos del Teclado para el usuario
+				
+				if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
+				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
+				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
+				if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
+				
+			}
+		}
+		
 	}
 	
 	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
 		
+		for(int i =0; i<handler.objeto.size();i++) { //tempObject es cualquier objecto en nuestro juego
+			GameObject tempObject = handler.objeto.get(i);
+			
+			if(tempObject.getId() == ID.Jugador) {
+				//Eventos del Teclado para el usuario
+				
+				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
+				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
+				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+				
+			}
+		}
 	}
 }
