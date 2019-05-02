94b1347273c88626737562c7326f495fa52fc2c5
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2019-05-02 00:07:37 -0500

    Teclado corregido

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -5,9 +5,14 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter{
 
 	private Handler handler;
+	private boolean[] keyDown = new boolean[4];
 	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		this.keyDown[0] =  false;
+		this.keyDown[1] =  false;
+		this.keyDown[2] =  false;
+		this.keyDown[3] =  false;
 	}
 	
 	public void keyPressed(KeyEvent e) {
@@ -19,10 +24,10 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getId() == ID.Jugador) {
 				//Eventos del Teclado para el usuario
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
+				if(key == KeyEvent.VK_W) { tempObject.setVelY(-5); this.keyDown[0] =  true;}
+				if(key == KeyEvent.VK_S) { tempObject.setVelY(5); this.keyDown[1] =  true;}
+				if(key == KeyEvent.VK_D) { tempObject.setVelX(5); this.keyDown[2] =  true;}
+				if(key == KeyEvent.VK_A) { tempObject.setVelX(-5); this.keyDown[3] =  true;}
 				
 			}
 		}
@@ -42,11 +47,19 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getId() == ID.Jugador) {
 				//Eventos del Teclado para el usuario
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_W) this.keyDown[0] =  false; //tempObject.setVelY(0);
+				if(key == KeyEvent.VK_S) this.keyDown[1] =  false; //tempObject.setVelY(0);
+				if(key == KeyEvent.VK_D) this.keyDown[2] =  false; //tempObject.setVelX(0);
+				if(key == KeyEvent.VK_A) this.keyDown[3] =  false; //tempObject.setVelX(0);
 				
+				//Movimiento vertical
+				if(!keyDown[0] && !keyDown[1]) {
+					tempObject.setVelY(0);
+				} 
+				//Movimiento horizontal
+				if(!keyDown[2] && !keyDown[3]) {
+					tempObject.setVelX(0);
+				} 
 			}
 		}
 	}
